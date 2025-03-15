06c5ec5de7b08977155291c003d5e6b794074c83
Dheepak G <83426515+DHEEPAK29@users.noreply.github.com>
2025-03-16 03:55:53 +0530

    Create baseFlask.py

---
+++ baseFlask.py
@@ -0,0 +1,38 @@
+from flask import Flask,render_template,request,url_for,session,redirect,jsonify
+import json,sqlite3
+from datetime import date
+import datetime
+from time import time
+from hashlib import sha256
+import datetime,time,pymongo
+from passlib.context import CryptContext
+import requests, ipfshttpclient
+import os,webbrowser
+from werkzeug.utils import secure_filename
+
+states=["Andhra Pradesh","Arunachal Pradesh ","Assam","Bihar","Chhattisgarh","Goa","Gujarat","Haryana","Himachal Pradesh","Jammu and Kashmir","Jharkhand","Karnataka","Kerala","Madhya Pradesh","Maharashtra","Manipur","Meghalaya","Mizoram","Nagaland","Odisha","Punjab","Rajasthan","Sikkim","Tamil Nadu","Telangana","Tripura","Uttar Pradesh","Uttarakhand","West Bengal","Andaman and Nicobar Islands","Chandigarh","Dadra and Nagar Haveli","Daman and Diu","Lakshadweep","NCT","Puducherry"]
+#session['user']='Genesis'
+'''Blockchain=[{
+        'index':'0',zzz
+        'patientid':'00000',
+        'first': '',
+        'last': '',
+        'doctor id': '',
+        'Dor': '12-13-2019',
+        'Age': '',
+        'haemo':'',
+        'blood':'',
+
+
+    }]'''
+
+login_status=0
+app= Flask(__name__)
+app.secret_key = 'PATREC Authentication'
+
+#Password encryption scheme
+pwd_context = CryptContext(
+        schemes=["pbkdf2_sha256"],
+        default="pbkdf2_sha256",
+        pbkdf2_sha256__default_rounds=30000
+)
